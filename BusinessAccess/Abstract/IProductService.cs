384ba93f094503bf6e1237848f1aad8c4d4939df
alibhs <alibahsi772@gmail.com>
2021-05-17 13:25:24 +0300

    Add project files.

---
+++ BusinessAccess/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessAccess.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+    }
+}
---
+++ BusinessAccess/Concrate/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessAccess.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessAccess.Concrate
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            return _productDal.GetAll();
+
+        }
+    }
+}

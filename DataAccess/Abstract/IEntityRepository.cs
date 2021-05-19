fed1d7916a096ad45a71b489c360afdb597f2718
alibhs <alibahsi772@gmail.com>
2021-05-20 02:54:27 +0300

    Update

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Expression link verebilmek icin. ||  filter = null filtre vermesin demek.
+    // generic constraint 
+    //class : referans tip
+    //IEntity : IEntity veya onu implamente eden bir nesne olabilir
+    //new() new'lenebilir olmalı (IEntity interface oldugu için newlenemez onu kullanamayız)
+
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T , bool>>  filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+    }
+}

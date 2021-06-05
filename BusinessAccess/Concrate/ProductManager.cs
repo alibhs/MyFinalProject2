e0b50352658b05571d485a1ee9f9b817c8c5d6d9
alibhs <alibahsi772@gmail.com>
2021-06-06 02:44:21 +0300

    10.Ders

--- BusinessAccess/Concrate/ProductManager.cs
+++ BusinessAccess/Concrate/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using BusinessAccess.Abstract;
+using BusinessAccess.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrate;
 using Entities.DTOs;
@@ -17,26 +19,48 @@ namespace BusinessAccess.Concrate
             _productDal = productDal;
         }
 
-        public List<Product> GetAll()
+        public IResult Add(Product product)
+        {
+            //business codes
+            if (product.ProductName.Length<2)
+            {
+                //magic string
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+
+            _productDal.Add(product);
+            return new Result(true,"ürün eklendi");
+        }
+
+        public IDataResult<List<Product>> GetAll()
         {
             //İş kodları
-            return _productDal.GetAll();
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
 
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p => p.CategoryId == id);
+            return new SuccessDataResult<List<Product>> (_productDal.GetAll(p => p.CategoryId == id));
         }
 
-        public List<Product> GetAllUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetAllUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return new SuccessDataResult<List<Product>> (_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<Product> GetById(int productId)
         {
-            return _productDal.GetProductDetails();
+            return new SuccessDataResult<Product> (_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            return new SuccessDataResult<List<ProductDetailDto>> (_productDal.GetProductDetails());
         }
     }
 }
---
+++ BusinessAccess/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessAccess.Constants
+{
+    //newlemekle ugrasmamak icin static yaptık
+   public static class Messages
+    {
+       public static string ProductAdded = "Ürün Eklendi";
+       public static string ProductNameInvalid = "Ürün ismi geçersiz";
+       public static string MaintenanceTime = "Sistem bakimda";
+       public static string ProductListed= "Ürünler Listelendi";
+    }
+}
